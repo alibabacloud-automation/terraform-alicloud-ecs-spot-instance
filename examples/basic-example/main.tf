22d26aa287817a8e24970060f457f0667266c5bf
LexWong <914088735@qq.com>
2020-03-04 17:22:53 +0800

    added(ecs-spot-instance): added ecs spot instance module.

---
+++ examples/basic-example/main.tf
@@ -0,0 +1,55 @@
+variable "profile" {
+  default = "default"
+}
+variable "region" {
+  default = "cn-hangzhou"
+}
+variable "zone_id" {
+  default = "cn-hangzhou-h"
+}
+
+provider "alicloud" {
+  region  = var.region
+  profile = var.profile
+}
+
+#############################################################
+# Data sources to get VPC, vswitch and default security group details
+#############################################################
+
+data "alicloud_vpcs" "default" {
+  is_default = true
+}
+
+data "alicloud_security_groups" "default" {
+  name_regex = "default"
+  vpc_id     = data.alicloud_vpcs.default.ids.0
+}
+
+data "alicloud_vswitches" "default" {
+  is_default = true
+  zone_id    = var.zone_id
+}
+
+// If there is no default vswitch, create one.
+resource "alicloud_vswitch" "default" {
+  count             = length(data.alicloud_vswitches.default.ids) > 0 ? 0 : 1
+  availability_zone = var.zone_id
+  vpc_id            = data.alicloud_vpcs.default.ids.0
+  cidr_block        = cidrsubnet(data.alicloud_vpcs.default.vpcs.0.cidr_block, 5, 3)
+}
+
+
+// ECS Module
+module "ecs_spot_instance" {
+  source                      = "../../"
+  region                      = var.region
+  profile                     = var.profile
+  instance_type_family        = "ecs.g6"
+  vswitch_id                  = length(data.alicloud_vswitches.default.ids) > 0 ? data.alicloud_vswitches.default.ids.0 : concat(alicloud_vswitch.default.*.id, [""])[0]
+  security_group_ids          = data.alicloud_security_groups.default.ids
+  associate_public_ip_address = true
+  internet_max_bandwidth_out  = 10
+  spot_strategy               = "SpotWithPriceLimit"
+  spot_price_limit            = 0.5
+}
