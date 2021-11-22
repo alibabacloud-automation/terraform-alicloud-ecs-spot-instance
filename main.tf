7df37bfc1af3e160e93150e4e785aac6dbbaaf83
liuyuan <709503583@qq.com>
2021-11-22 10:59:44 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/ecs-spot-instance"
-}
-
 data "alicloud_instance_types" "this" {
   instance_type_family = var.instance_type_family != "" ? var.instance_type_family : null
   instance_charge_type = "PostPaid"
